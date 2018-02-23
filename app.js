62a5a6f27eeca861cf9bbb3cca5cbb5a99674951
Mounika <212687639@logon.ds.ge.com>
2018-02-23 17:06:44 +0530

    added port

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var router = require('./routes/router.js');
+const PORT = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({
   extended : true
 }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(router);
-app.listen(8000,function(){
-  console.log("ping me on 8000!");
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
